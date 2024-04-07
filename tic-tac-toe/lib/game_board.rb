c1745b025b4f8d1b83d153461c757f15c9807aec
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-04-07 15:16:41 -0400

    Implement game board, initial state, state reset, and add_move

--- tic-tac-toe/lib/game_board.rb
+++ tic-tac-toe/lib/game_board.rb
@@ -1,24 +1,48 @@
 
 class GameBoard
-  attr_accessor :state
+  # Purpose: control the state of the gameboard display
+  # API provides read-access to @state and requests to #update_board
+  
+  attr_reader :state
+
+  SYM = {
+    :VL => "\u2502",    # unicode vertical line
+    :HL => "\u2500",    # unicode horizontal line
+    :cross => "\u253c", # unicode cross
+    :space => " "
+  }
+
+  INIT_STATE = [
+    [SYM[:space], SYM[:VL], SYM[:space], SYM[:VL], SYM[:space]],
+    [SYM[:HL], SYM[:cross] ,SYM[:HL], SYM[:cross], SYM[:HL]],
+    [SYM[:space], SYM[:VL], SYM[:space], SYM[:VL], SYM[:space]],
+    [SYM[:HL], SYM[:cross] ,SYM[:HL], SYM[:cross], SYM[:HL]],
+    [SYM[:space], SYM[:VL], SYM[:space], SYM[:VL], SYM[:space]]
+  ]
 
   def initialize
+    reset_board()
   end
   
-  # Public  
   def update_board(cmd=nil, player=nil, pos=nil)
     if cmd == :add
       add_move(player, pos)
-    else
+    elsif cmd == :reset
       reset_board()
+    else
+      # HANDLE INCORRECT COMMAND PASSED
     end
   end
 
   private
   
   def add_move(player, pos)
+    row = pos[0]
+    col = pos[1]
+    @state[row][col] = player
   end
 
   def reset_board
+    @state = INIT_STATE.map { |row| row }
   end
 end
