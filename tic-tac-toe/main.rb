e8d1b7a6f5d97046be18a39c82e30d5b88d5dad1
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-04-07 15:18:03 -0400

    Implement position hash and game program logic

--- tic-tac-toe/main.rb
+++ tic-tac-toe/main.rb
@@ -1,31 +1,44 @@
 require_relative "lib/game_board.rb"
 
 class Game
+  attr_reader :player_x, :player_o
+
   ADD = :add
   POS = {
-    "TL" => [0, 0],
-    "TM",
-    "TR",
-    "ML",
-    "MM",
-    "MR",
-    "BL",
-    "BM",
-    "BR"
+    "TL" => [0, 0], # top left
+    "TM" => [0, 2], # top middle
+    "TR" => [0, 4], # top right
+    "ML" => [2, 0], # ...
+    "MM" => [2, 2],
+    "MR" => [2, 4],
+    "BL" => [4, 0],
+    "BM" => [4, 2],
+    "BR" => [4, 4]
   }
 
   def initialize
+    @player_x = "X"
+    @player_o = "O"
   end
 
   def get_board(game_board)
     game_board.state
   end
-
-  def prompt_start
-  end
 end
 
+
+# Program
 game_board = GameBoard.new
 game = Game.new
-
-game.play(game_board)
+loop do
+  print "Would you like to play tic-tac-toe? (y/n): "
+  start = gets.chomp
+  if start.downcase == "y"
+    # play game
+  elsif start.downcase == "n"
+    puts "Thanks for playing!"
+    exit
+  else
+    puts "Unknown Command '#{start}'"
+  end
+end
