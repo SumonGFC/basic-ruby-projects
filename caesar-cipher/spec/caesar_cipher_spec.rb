4e506b59b1837900209a434da971aaa9aa245840
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-06-28 14:40:13 -0400

    Caesar Cipher: Implement first test

---
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../caesar-cipher'
+
+describe 'Caesar Cipher' do
+  describe '#rot_codepoint' do
+    it 'returns nil if either input is not an integer' do
+      expect(rot_codepoint('a', nil)).to eq(nil)
+      expect(rot_codepoint('a', 1)).to eq(nil)
+      expect(rot_codepoint(10, [])).to eq(nil)
+    end
+  end
+
+  describe '#caesar-cipher' do
+  end
+end
+
+# rot_codepoint:
+# - input types
+# - input ranges
+# - shift properly (no wrap) (lower + upper)
+# - shift properly (wrap) (lower + upper)
+# - no shift for code points outside of range
