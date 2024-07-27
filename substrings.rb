bfdaf351701aa6ff801e78ec83d32e0f12c4af0e
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-07-27 14:48:04 -0400

    Reformat code to look nicer

--- substrings.rb
+++ substrings.rb
@@ -1,25 +1,28 @@
-# Implement method #substrings: 
+# Implement method #substrings:
 # input: str, array of strs.
 # return: hash listing the indices of each substring that was found; case sensitive
 
 def substrings(str, substr_arr)
   result = {}
 
-  substr_arr.each do |substr| #for each word in the dictionary
+  substr_arr.each do |substr|
     indices = []
-    match_pos = str.index(substr, 0)  #find index of first match of substr in the string we are testing
-    while !match_pos.nil? # str.index will return nil if no matches found
-      indices.push(match_pos) # push index of the match
-      match_pos = str.index(substr, match_pos + 1)  #get index of next match, starting from after the previous match
+    # find index of first match of substr in the string we are testing
+    match_pos = str.index(substr, 0)
+    # str.index will return nil if no matches found
+    until match_pos.nil?
+      # push index of the match
+      indices.push(match_pos)
+      # get index of next match, starting from after the previous match
+      match_pos = str.index(substr, match_pos + 1)
     end
-    result[substr] = indices unless indices.empty?  #the length of indices array is the number of matches found
+    result[substr] = indices unless indices.empty?
   end
-  return result
+  result
 end
 
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-x = substrings("below logogogoingwlhowdy low", dictionary)
+x = substrings('below logogogoingwlhowdy low', dictionary)
 
 p x
